a1e07d25e0c2adf7ce46c087596b660957ce69d3
Rodrigo Andrade <rodrigoacm10@gmail.com>
2025-07-25 20:35:40 -0300

    add: frame and style base

--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,85 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { invoke } from '@tauri-apps/api/core'
 import './App.css'
+import { useForm } from 'react-hook-form'
+import Viewer from './components/Viewer'
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState('')
-  const [name, setName] = useState('')
-
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke('greet', { name }))
+    // setGreetMsg(await invoke('greet', { name }))
+  }
+
+  const [iframeOpen, setIframeOpen] = useState(false)
+
+  const { register, handleSubmit, setValue, watch } = useForm()
+
+  const onSubmit = (values: any) => {
+    // https://www.youtube.com/watch?v=4iN84gLicnM -> errado
+    // https://www.youtube.com/embed/rggg1mrQTMY -> certo
+
+    // https://www.twitch.tv/baiano - errado
+    // https://player.twitch.tv/?channel=baiano&parent=localhost -> certo video
+    // https://www.twitch.tv/embed/baiano/chat?parent=localhost -> certo chat
+
+    // https://docs.google.com/document/d/1aJTx22S6WyqoNE6prNiU0qtRRtrP4QqJFEsbw8BKppA/edit?usp=sharing
+    console.log('Values', values)
+    setIframeOpen(true)
   }
 
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    <main className="min-h-screen min-w-screen bg-red-300 flex">
+      <div className="flex-1 bg-black flex items-center justify-center flex-col">
+        {!iframeOpen && (
+          <>
+            <h1 className="text-white bg-red-500 text-center">
+              Insira uma URL
+            </h1>
+            <form
+              className="bg-blue-300 w-full flex flex-col items-center justify-center"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <input
+                {...register('url')}
+                placeholder="Ex: https://youtube.com/..."
+                className="bg-red-200 rounded-md px-4 py-2"
+              />
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+              <div className="flex gap-2">
+                <button
+                  type="button"
+                  onClick={() => {
+                    console.log(
+                      'quando apertar aki, vai colocar colocar o input "type" = youtube',
+                    )
+                    setValue('type', 'youtube')
+                    // Exemplo: setValue('type', 'youtube')
+                  }}
+                  className="cursor-pointer flex-1 p-2 bg-red-400 rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+                >
+                  <div className="text-white text-center">
+                    <h2 className="font-bold text-lg">Youtube</h2>
+                    <p className="text-sm">insira url de um vídeo</p>
+                  </div>
+                </button>
+              </div>
+            </form>
+          </>
+        )}
+
+        {/* {iframeOpen && (
+          <div className="">
+            <iframe
+              className="w-screen h-screen"
+              src="https://www.youtube.com/embed/rggg1mrQTMY"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        )} */}
+        {iframeOpen && <Viewer iframeOpen={iframeOpen} />}
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault()
-          greet()
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
     </main>
   )
 }
